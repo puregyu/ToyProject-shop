581b94cdc06b2cdba094ddbbde88b37c070dc97f
puregyu <puregyu@naver.com>
2020-01-10 17:49:16 +0900

    web, thymeleaf, H2, lombook 초기세팅

---
+++ jpashop/jpashop/src/main/java/com/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager; 
+	
+	public Long save(Member member) {
+		entityManager.persist(member);
+		return member.getId();
+	}
+	
+	public Member find(Long id) {
+		return entityManager.find(Member.class, id);
+	}
+}
